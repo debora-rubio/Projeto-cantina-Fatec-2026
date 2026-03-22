c2d2b466613b666a9b362e027c50420239d8cf46
debora-rubio <debora.rubio@fatec.sp.gov.br>
2026-03-21 23:30:14 -0300

    Substitui no código getter e setter por @property e @setter, criei a classe ItemConsumo

--- cantinafatec2026.py
+++ cantinafatec2026.py
@@ -1,11 +1,20 @@
 # Acesso Dev (desenvolvedores).
+
 # Iniciando a criação de uma classe Produto. Cada produto da cantina será um objeto dessa classe.
+
+# Os Atributos (variáveis) da classe Produto são: nome, preço de compra, preço de venda, data de compra, vencimento e quantidade.
+
+# Os métodos (ações/funções) da classe Produto.
+
 # _init_ é o metodo que executa quando o objeto é criado.
-# self representa o proprio objeto, ou seja, o produto que esta sendo criado.
+
+# self representa o objeto atual que está chamando o método.
+
 # Incluí a proteção Getters(leitura) e Setters(atributo protegico com permissão de alteração restrita) no codigo,
 # para proteção dos dados, assim os atributos (nome, preço, data, vencimento e quantidade)
 # ficam privados, podendo ser alterados somente quando autorizado, evitando erros ou alterações indevidas.
 
+# Para melhoria do código, substitui o getters e setters por @property e @setter, deixando o codigo mais limpo.
 
 
 class Produto:
@@ -17,23 +26,51 @@ class Produto:
         self.__vencimento = vencimento
         self.__quantidade = quantidade
 
-    # Getters
-    def get_nome(self): return self.__nome
-    def get_preco_compra(self): return self.__preco_compra
-    def get_preco_venda(self): return self.__preco_venda
-    def get_data_compra(self): return self.__data_compra
-    def get_vencimento(self): return self.__vencimento
-    def get_quantidade(self): return self.__quantidade
+    @property
+    def nome(self):
+        return self.__nome
 
-    # Setters
-    def set_preco_compra(self, novo_preco):
-        if novo_preco >= 0: self.__preco_compra = novo_preco
-    def set_preco_venda(self, novo_preco):
-        if novo_preco >= 0: self.__preco_venda = novo_preco
-    def set_quantidade(self, nova_qtd):
-        if nova_qtd >= 0: self.__quantidade = nova_qtd
+    @property
+    def preco_compra(self):
+        return self.__preco_compra
 
-    # Baixar estoque
+    @preco_compra.setter
+    def preco_compra(self, novo_preco):
+        if novo_preco >= 0:
+            self.__preco_compra = novo_preco
+        else:
+            print("Preço de compra inválido!")
+
+    @property
+    def preco_venda(self):
+        return self.__preco_venda
+
+    @preco_venda.setter
+    def preco_venda(self, novo_preco):
+        if novo_preco >= 0:
+            self.__preco_venda = novo_preco
+        else:
+            print("Preço de venda inválido!")
+
+    @property
+    def data_compra(self):
+        return self.__data_compra
+
+    @property
+    def vencimento(self):
+        return self.__vencimento
+
+    @property
+    def quantidade(self):
+        return self.__quantidade
+
+    @quantidade.setter
+    def quantidade(self, nova_qtd):
+        if nova_qtd >= 0:
+            self.__quantidade = nova_qtd
+        else:
+            print("Quantidade inválida!")
+
     def consumir(self, qtd):
         if qtd <= self.__quantidade:
             self.__quantidade -= qtd
@@ -41,4 +78,19 @@ class Produto:
         else:
             print("Estoque insuficiente!")
             return False
+        
+class ItemConsumo:
+    def __init__(self, produto, quantidade):
+        self.__produto = produto
+        self.__quantidade = quantidade
 
+    @property
+    def produto(self):
+        return self.__produto
+
+    @property
+    def quantidade(self):
+        return self.__quantidade
+
+    def calcular_subtotal(self):
+        return self.__produto.preco_venda * self.__quantidade
